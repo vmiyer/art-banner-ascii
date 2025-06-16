ff8c0febed44f663530a0f1a16518ef4c1819aae
vmiyer <Varun.Iyer@genmills.com>
2025-06-16 15:30:29 +0530

    test

--- tests/test_banner.py
+++ tests/test_banner.py
@@ -2,5 +2,6 @@ from ascii_art_banner.banner import generate
 
 def test_generate():
     result = generate("Hello")
+    print(result)
     assert "Hello" not in result  # Should be rendered in ASCII art
     assert len(result) > 0
